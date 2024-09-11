2b0693dc2076e051b282290a6dae733f4fb014ce
srangseethammakul <srangseethammakul@gmail.com>
2024-09-11 22:18:05 +0700

    update

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -13,24 +13,20 @@ class StockController extends Controller
 		$perPage = $request->get('per_page', 10); // Get per_page from the DataTables request
 		$offset = ($page - 1) * $perPage;
 		$draw = $request->get('draw');
-        $query = null;
-        $stocks = Stock_transfer::with(['getSource', 'getDestination'])        
-            ->orderBy('status', 'asc')
-            ->orderBy('id', 'desc');
+        $totalRecords = Stock_transfer::count();
 
-        if($query){
-            $users = User::where('brand_name', 'like', '%'.$query.'%')->get();
-            $stocks = $stocks->whereIn('dst_id', $users->pluck('id')->toArray());
-        }
-
-        $stocks = $stocks
+        $stocks = Stock_transfer::orderBy('status', 'asc')
+            ->orderBy('id', 'desc')
             ->skip($offset)
             ->take($perPage)
+            ->with(['getSource', 'getDestination'])
             ->get();
+
+
         return response()->json([
             'draw' => intval($draw), // Draw counter
-            'recordsTotal' => $stocks->count(), // Total records
-            'recordsFiltered' => $stocks->count(), // Filtered records count
+            'recordsTotal' => $totalRecords, // Total records
+            'recordsFiltered' => $totalRecords, // Filtered records count
             'data' => $stocks // Data array
         ]);
 	}
